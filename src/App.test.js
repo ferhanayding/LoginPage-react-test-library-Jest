739bdaf51170d875b43a2aba9c4ea49221f6f3c6
ferhanayding <ferhanaydin099@gmail.com>
2023-02-03 11:12:28 +0300

    first commit!

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test the App Component", () => {
+  test("header renders with react testing tutorial in the document", () => {
+    const component = render(<App />);
+    const titleElement = component.getByText(/Burası test etme mekanı/i);
+    const helloElement = component.getByText(/hello/i);
+    expect(helloElement).toBeInTheDocument();
+    expect(titleElement).toBeInTheDocument();
+  });
+  test("render login component in doucment", () => {
+    const { getByLabelText } = render(<App />);
+    const childElement = getByLabelText("Email");
+    expect(childElement).toBeTruthy();
+  });
 });
